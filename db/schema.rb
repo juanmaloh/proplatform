fc3823e16ee7c86cb31fa57da1490cb83a301b1f
Juanloh <juananue0@gmail.com>
2024-08-21 09:28:50 -0500

    Creamos un controlador para las vistas después del login

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_213343) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_21_133804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "forms", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_forms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_20_213343) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "forms", "users"
 end
